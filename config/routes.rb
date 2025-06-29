f665cf9a63d8899ea6be5e690b7fe794ead7c177
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-06-29 08:13:49 +0000

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'  # データのタイトル一覧表示
   # get 'lists/show'
   get 'lists/:id' => 'lists#show', as: 'list' # 各idの詳細データ
-  get 'lists/edit'
+  # get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list' # 各idの編集アドレス
+  patch 'lists/:id' => 'lists#update', as: 'update_list' # updateアクションの追加
   get '/top' => "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
